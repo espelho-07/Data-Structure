370f8247cc74867197e69ff1df85988ce67b4300
espelho-07 <darpanparmar1707@gmail.com>
2025-08-22 11:10:31 +0530

    PRE POST SE IN

--- LAB-18/18A.c
+++ LAB-18/18A.c
@@ -2,42 +2,46 @@
 #include <stdlib.h>
 
 // Node structure
-struct Node {
+struct Node
+{
     int data;
     struct Node *left;
     struct Node *right;
 };
 
 // Create a new node
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     newNode->data = data;
     newNode->left = newNode->right = NULL;
     return newNode;
 }
 
 // Function to construct tree from preorder and postorder
-struct Node* constructTree(int pre[], int post[], int postStart, int postEnd, int size, int preIndex) {
-    // If index out of bounds or invalid range
+struct Node *constructTree(int pre[], int post[], int postStart, int postEnd, int size, int preIndex)
+{
+
     if (preIndex >= size || postStart > postEnd)
         return NULL;
 
-    // Create root node with current element
-    struct Node* root = createNode(pre[preIndex]);
+ 
+    struct Node *root = createNode(pre[preIndex]);
 
-    // If only one node or end reached
+
     if (postStart == postEnd || preIndex == size - 1)
         return root;
 
-    // Find next preorder element in postorder[]
     int i;
-    for (i = postStart; i <= postEnd; i++) {
+    for (i = postStart; i <= postEnd; i++)
+    {
         if (post[i] == pre[preIndex + 1])
             break;
     }
 
-    // Construct left and right subtrees
-    if (i <= postEnd) {
+    // Construct left and right subtrees Using Recursion
+    if (i <= postEnd)
+    {
         root->left = constructTree(pre, post, postStart, i, size, preIndex + 1);
         root->right = constructTree(pre, post, i + 1, postEnd - 1, size, preIndex + (i - postStart + 2));
     }
@@ -46,20 +50,23 @@ struct Node* constructTree(int pre[], int post[], int postStart, int postEnd, in
 }
 
 // Print inorder traversal
-void printInorder(struct Node* root) {
-    if (root == NULL) return;
+void printInorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
     printInorder(root->left);
     printf("%d ", root->data);
     printInorder(root->right);
 }
 
 // Main function
-int main() {
-    int pre[]  = {1, 2, 4, 5, 3, 6, 7};
+int main()
+{
+    int pre[] = {1, 2, 4, 5, 3, 6, 7};
     int post[] = {4, 5, 2, 6, 7, 3, 1};
     int size = 7;
 
-    struct Node* root = constructTree(pre, post, 0, size - 1, size, 0);
+    struct Node *root = constructTree(pre, post, 0, size - 1, size, 0);
 
     printf("Inorder traversal of constructed tree:\n");
     printInorder(root);
